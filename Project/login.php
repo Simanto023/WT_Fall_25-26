0acf483fe0a4409edba4f2b5270b83ae4206ba31
Saidur Rahman <23-51985-2@student.aiub.edu>
2026-01-03 16:00:35 +0600

    Session added in login and redirect using role

--- Project/login.php
+++ Project/login.php
@@ -1,5 +1,18 @@
 <?php
 include "PHP/login_logic.php";
+session_start();
+
+$_SESSION['user_id'] = $row['id'];
+$_SESSION['role'] = $row['role'];
+$_SESSION['full_name'] = $row['full_name'];
+
+
+if ($row['role'] == 'admin') {
+    header("Location: ../admin_dashboard.php");
+} else {
+    header("Location: ../customer_dashboard.php");
+}
+exit;
 ?>
 <!DOCTYPE html>
 <html>
